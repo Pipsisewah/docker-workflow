98f84d3bea659c7d45b3056954ffff74e8c9b8a5
Pipsisewah <stevenanthonylopes@gmail.com>
2024-05-16 09:05:28 -0500

    Migrating to a new dynamic workflow

--- composer.js
+++ composer.js
@@ -1,18 +1,52 @@
 const workflowComposer = require('./workflowComposer');
+const { Machine, assign, sendParent, send } = require('xstate');
+const dockerActions = require('./actions/dockerActions');
+const nginxActions = require("./actions/nginxActions");
+const mainStateMachine = require("./stateMachines/main");
 
-const workflow = workflowComposer.readWorkflow('firstWorkflow').then((workflow) => {
-    if(workflow.meta.type === 'failure'){
-        console.log('Failed to import workflow');
-        console.log(workflow.meta.type);
-        return;
+async function pullImages(images) {
+    for (const image of images) {
+        console.log(`Pulling image: ${image}`);
+        await actions.pullImage(image);
     }
+    console.log('Images have been pulled');
+}
 
-    for (const step of workflow.workflow){
-        console.log(step);
-        
+
+
+
+const actions = {
+    pullImage: dockerActions.pullImage,
+    startContainer: dockerActions.startContainer,
+};
+
+const services = {};
+
+
+const dockerScriptMachine = Machine(
+    mainStateMachine,
+    {
+        actions,
+        services,
     }
+);
 
+function compileListOfRequiredImages(workflow) {
+    return workflow
+        .filter(step => step.type === 'container')
+        .map(step => step.config.dockerImage);
+}
 
-})
+async function main() {
+    const workflowDefinition = await workflowComposer.readWorkflow('firstWorkflow');
+    //console.log(JSON.stringify(workflowDefinition));
+    const requiredDockerImages = compileListOfRequiredImages(workflowDefinition.workflow);
+    await pullImages(requiredDockerImages);
+}
 
+main().then(() => {
+    console.log('Main function completed!');
+}).catch(error => {
+    console.error('Error!, error');
+});
 
--- workflowComposer.js
+++ workflowComposer.js
@@ -5,22 +5,37 @@ const workflowComposer = {};
 
 const readFileAsync = util.promisify(fs.readFile);
 
+workflowComposer.createDynamicStateMachineStructure = (workflow) => {
+    workflow.workflow.forEach((flow, index) => {
+        //console.log(`Reviewing ${JSON.stringify(flow)}`)
+        if( index < workflow.length -1) {
+            flow.on = {NEXT: workflow[index + 1].name}
+        }else{
+            flow.on = {end: {type: 'final'}}
+        }
+    })
+    return workflow;
+}
+
 workflowComposer.readWorkflow = async (workflowName) => {
     const workflowPath = './workflows/' + workflowName + '.json';
     try {
         // Read the file asynchronously
-        const data = await readFileAsync(workflowPath, 'utf8');
+        const data = await readFileAsync(workflowPath);
 
         // Parse the JSON data
-        const jsonData = JSON.parse(data);
-        console.log('Parsed JSON data:', jsonData);
+        const jsonData = JSON.parse(data.toString());
+        //console.log('Parsed JSON data:', jsonData);
 
-        return jsonData;
+
+        return workflowComposer.createDynamicStateMachineStructure(jsonData);
     } catch (error) {
         console.error('An error occurred:', error);
         throw error; // Re-throw the error to handle it outside the function if needed
     }
 }
+
+
 
 
 module.exports = workflowComposer;
